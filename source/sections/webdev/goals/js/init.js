ca5d432d2efecac9360f4501d95def59cc1597c3
Chris Jones <chris@chrismjon.es>
2024-09-02 20:34:37 -0400

    updating html validator script

--- source/sections/webdev/goals/js/init.js
+++ source/sections/webdev/goals/js/init.js
@@ -31,6 +31,25 @@ function init() {
   document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
   document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
 
+
+  // Call the HTML validator API and add the result to the footer
+  fetch("https://html5.validator.nu/?out=json&doc=" + encodeURIComponent(loc), {
+      method: "GET"
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.messages.length === 0) {
+          console.log("No Errors");
+          document.getElementById("HTMLCSS").innerHTML = "<strong> HTML/CSS </strong> Valid!";
+      } else {
+          console.log("Errors");
+          console.log(data.messages); // data.messages is an array
+          document.getElementById("HTMLCSS").innerHTML = "<strong> HTML/CSS </strong> NOT Valid!";
+      }
+  })
+  .catch(error => {
+      console.warn(error);
+  });
   //var conv = new showdown.Converter();
   //var md_txt = document.getElementById('md_content').textContent;
   //console.log(md_txt);
