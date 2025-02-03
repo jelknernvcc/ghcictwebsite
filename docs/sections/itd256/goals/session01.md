b1dbe436a801e199c1496a0912dbf018f457590a
Chris Jones <christopher.jones@apsva.us>
2025-02-03 14:35:16 -0500

    setup db course, add inital lesson plan

---
+++ docs/sections/itd256/goals/session01.md
@@ -0,0 +1,20 @@
+## Lesson Objectives
+By the end of this lesson, you should:
+- **Understand** that query performance can vary wildly depending on the exact
+execution process.
+- **Understand** that database execution is complex, and that there are a lot of
+strategies that database engines use to optimize query performance.
+
+
+## What We'll Do In Class
+
+### Play Cards
+We'll use a deck of cards to represent a database. We'll practice:
+- Selects (finding cards in your deck)
+- Inserts (adding new cards to your deck)
+- Deletes (removing cards from your deck)
+
+I've written a bunch of Postgres queries that we'll go through [here](../resources/card_sorting.sql)
+
+## Homework
+None! We'll introduce a new activity related to database queries on Thursday
---
+++ docs/sections/itd256/resources/card_sorting.sql
@@ -0,0 +1,137 @@
+
+
+
+
+
+
+
+
+CREATE TYPE suit 
+AS ENUM ('heart', 'diamond', 'club', 'spade');
+
+
+CREATE TYPE rank 
+AS ENUM ('A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K');
+
+
+
+CREATE TYPE color 
+AS ENUM ('red', 'blue');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE your_cards (
+    rank rank,
+    suit suit,
+    back_color color
+);
+
+
+
+
+INSERT INTO your_cards(rank, suit, back_color)
+SELECT rank, suit, back_color
+FROM all_cards
+ORDER BY RANDOM()
+LIMIT 30;
+
+
+
+
+
+
+
+SELECT COUNT(*) FROM your_cards WHERE rank = '7';
+
+
+
+
+
+SELECT COUNT(*) FROM your_cards WHERE rank = '7';
+
+
+
+
+
+SELECT COUNT(*) FROM your_cards WHERE rank = '8';
+
+
+
+
+
+
+CREATE INDEX idx_rank ON your_cards(rank);
+
+
+
+
+
+SELECT count(*) FROM your_cards WHERE rank = 'A';
+
+
+
+
+
+SELECT suit, count(*) FROM your_cards WHERE rank = '7';
+
+
+
+
+
+CLUSTER your_cards USING idx_rank;
+
+
+
+
+
+
+INSERT INTO your_cards (rank, suit, back_color) VALUES ('7', 'hearts', 'red');
+DELETE FROM your_cards WHERE rank = 'A';
+
+
+
+
+
+
+
+CREATE TABLE your_cards (
+    rank rank,
+    suit suit,
+    back_color color
+) PARTITION BY LIST (back_color);
+
+CREATE TABLE your_cards_red PARTITION OF your_cards FOR VALUES IN ('red');
+CREATE TABLE your_cards_blue PARTITION OF your_cards FOR VALUES IN ('blue');
+
+
+
+--Run these next two queries in parallel
+SELECT COUNT(*) FROM your_cards WHERE rank = '7';
+
+
+
+
+SELECT rank, suit FROM your_cards WHERE rank = '7' GROUP BY rank, suit HAVING COUNT(*) > 1;
+
+
+
+
+
+
+--JOIN
+SELECT y.rank, o.rank
+FROM your_cards y
+JOIN opponent_cards o
+ON y.rank = o.rank;
+
